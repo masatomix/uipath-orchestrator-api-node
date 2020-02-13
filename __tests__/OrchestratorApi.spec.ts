0e2d3e7bb698d3ec3cdb02b55158098d49c2f3e5
Masatomi KINO <masatomix@ki-no.org>
2020-02-14 01:16:50 +0900

    Queue追加。 findAllもOptionalな引数を追加。(GETのパラメタとして)

--- __tests__/OrchestratorApi.spec.ts
+++ __tests__/OrchestratorApi.spec.ts
@@ -3,129 +3,149 @@ import OrchestratorApi from '../src/index'
 
 import config from 'config'
 
-const api = new OrchestratorApi(config)
-
 // async function main() {
 //   const robot = await api.robot.find(1)
 //   robot.Description = 'test3'
 //   await api.robot.update(robot)
 // }
 
-beforeEach(async () => {
-  await api.authenticate()
-})
-
 describe('OrchestratorApi のテスト', () => {
-  it('license のテスト', async () => {
-    // ライセンスを取得する
-    const license: any = await api.license.find()
-    expect(license.Allowed).not.toBeUndefined()
-    expect(license.Used).not.toBeUndefined()
-    console.log(license.Allowed)
-    console.log(license.Used)
+  const api = new OrchestratorApi(config)
+
+  beforeEach(async () => {
+    await api.authenticate()
   })
 
-  it('Robot のテスト', async () => {
-    // // ロボットを取得する
-    const instances: any[] = await api.robot.findAll()
-    expect(instances.length).toBeGreaterThanOrEqual(1)
+  describe('OrchestratorApi のテスト', () => {
+    it('license のテスト', async () => {
+      // ライセンスを取得する
+      const license: any = await api.license.find()
+      expect(license.Allowed).not.toBeUndefined()
+      expect(license.Used).not.toBeUndefined()
+      console.log(license.Allowed)
+      console.log(license.Used)
+    })
 
-    for (const instance of instances) {
-      const robotId: number = instance.Id
-      const robot = await api.robot.find(robotId)
-      // console.log('RobotId:', robot.Id)
-      expect(robot.Id).toBe(robotId)
-    }
-  })
+    it('Robot のテスト', async () => {
+      // // ロボットを取得する
+      const instances: any[] = await api.robot.findAll()
+      expect(instances.length).toBeGreaterThanOrEqual(1)
 
-  describe('User登録テスト', () => {
-    const user = {
-      Name: 'user002',
-      Surname: 'LastName002',
-      UserName: 'てすと',
-      FullName: 'User002_LastName002',
-      EmailAddress: 'masatomix@example.com',
-      IsActive: true,
-      Password: 'afjlaf#adA0!',
-      RolesList: ['Robot'],
-      // TenantId: 1,
-    }
-    it('User登録/更新テスト', async () => {
-      try {
-        // ユーザ作成
-        let testUser = await api.user.create(user)
-        const testUserId = testUser.Id
-        expect(testUser.UserName).toBe(user.UserName) // 戻り値がテストユーザと同じであること
-
-        const expectedName: string = 'テストさん'
-        // 検索する
-        testUser = await api.user.find(testUserId)
-        testUser.Name = expectedName
-        // 更新をしてみる
-        testUser = await api.user.update(testUser)
-        // updateは戻り値は空
-        expect(testUser).toBeUndefined()
-
-        testUser = await api.user.find(testUserId)
-        expect(testUser.Name).toBe(expectedName)
-      } catch (error) {
-        console.log('Community版の場合は失敗するかもしれない')
-        fail(error)
+      for (const instance of instances) {
+        const robotId: number = instance.Id
+        const robot = await api.robot.find(robotId)
+        // console.log('RobotId:', robot.Id)
+        expect(robot.Id).toBe(robotId)
       }
     })
 
-    afterEach(async () => {
-      try {
-        const result: any[] = await api.user.findByUserName(user.UserName)
-        expect(result.length).toBe(1)
-        expect(result[0].UserName).toBe(user.UserName)
+    describe('User登録テスト', () => {
+      const user = {
+        Name: 'user002',
+        Surname: 'LastName002',
+        UserName: 'てすと',
+        FullName: 'User002_LastName002',
+        EmailAddress: 'masatomix@example.com',
+        IsActive: true,
+        Password: 'afjlaf#adA0!',
+        RolesList: ['Robot'],
+        // TenantId: 1,
+      }
+      if (api.isEnterprise) {
+        it('User登録/更新テスト', async () => {
+          try {
+            // ユーザ作成
+            let testUser = await api.user.create(user)
+            const testUserId = testUser.Id
+            expect(testUser.UserName).toBe(user.UserName) // 戻り値がテストユーザと同じであること
 
-        await api.user.delete(result[0].Id)
-      } catch (error) {
-        fail(error)
+            const expectedName: string = 'テストさん'
+            // 検索する
+            testUser = await api.user.find(testUserId)
+            testUser.Name = expectedName
+            // 更新をしてみる
+            testUser = await api.user.update(testUser)
+            // updateは戻り値は空
+            expect(testUser).toBeUndefined()
+
+            testUser = await api.user.find(testUserId)
+            expect(testUser.Name).toBe(expectedName)
+          } catch (error) {
+            console.log('Community版の場合は失敗するかもしれない')
+            fail(error)
+          }
+        })
+      } else {
+        console.log('Community版のため、スキップ')
+      }
+
+      afterEach(async () => {
+        try {
+          const result: any[] = await api.user.findByUserName(user.UserName)
+          expect(result.length).toBe(1)
+          expect(result[0].UserName).toBe(user.UserName)
+
+          await api.user.delete(result[0].Id)
+        } catch (error) {
+          fail(error)
+        }
+      })
+    })
+
+    it('Machine のテスト', async () => {
+      // Machineを取得する
+      const instances = await api.machine.findAll()
+      expect(instances.length).toBeGreaterThanOrEqual(1)
+
+      for (const instance of instances) {
+        // console.log(instance)
+        expect(instance.Name).not.toBeUndefined()
       }
     })
-  })
 
-  it('Machine のテスト', async () => {
-    // Machineを取得する
-    const instances = await api.machine.findAll()
-    expect(instances.length).toBeGreaterThanOrEqual(1)
+    it('Process のテスト', async () => {
+      const instances = await api.process.findAll()
+      expect(instances.length).toBeGreaterThanOrEqual(0)
 
-    for (const instance of instances) {
-      // console.log(instance)
-      expect(instance.Name).not.toBeUndefined()
-    }
-  })
+      for (const instance of instances) {
+        // console.log(instance)
+        expect(instance.ProcessKey).not.toBeUndefined()
+      }
+    })
 
-  it('Process のテスト', async () => {
-    const instances = await api.process.findAll()
-    expect(instances.length).toBeGreaterThanOrEqual(0)
+    it('Schedules のテスト', async () => {
+      const instances = await api.schedule.findAll()
+      expect(instances.length).toBeGreaterThanOrEqual(0)
 
-    for (const instance of instances) {
-      // console.log(instance)
-      expect(instance.ProcessKey).not.toBeUndefined()
-    }
-  })
+      for (const instance of instances) {
+        // console.log(instance)
+        expect(instance.ReleaseKey).not.toBeUndefined()
+      }
+    })
 
-  it('Schedules のテスト', async () => {
-    const instances = await api.schedule.findAll()
-    expect(instances.length).toBeGreaterThanOrEqual(0)
+    it('Queue のテスト', async () => {
+      const instances = await api.queue.findAll()
+      expect(instances.length).toBeGreaterThanOrEqual(0)
 
-    for (const instance of instances) {
-      // console.log(instance)
-      expect(instance.ReleaseKey).not.toBeUndefined()
-    }
-  })
+      for (const instance of instances) {
+        // console.log(instance)
+        expect(instance.Id).not.toBeUndefined()
+      }
+      const queueItemId = instances[0].Id
+      const result = await api.queue.find(queueItemId)
+      console.log(result)
+      expect(result.Id).toBe(queueItemId)
+    })
 
-  it('汎用メソッド のテスト', async () => {
-    const instances = await api.getArray('/odata/Folders')
-    expect(instances.length).toBeGreaterThanOrEqual(0)
+    it('汎用メソッド のテスト', async () => {
+      const instances = await api.getArray('/odata/Folders')
+      expect(instances.length).toBeGreaterThanOrEqual(0)
 
-    for (const instance of instances) {
-      // console.log(instance)
-      expect(instance.DisplayName).not.toBeUndefined()
-    }
+      for (const instance of instances) {
+        // console.log(instance)
+        expect(instance.DisplayName).not.toBeUndefined()
+      }
+    })
   })
 
   describe('Enterprise判別テスト。と認証エラー系', () => {
