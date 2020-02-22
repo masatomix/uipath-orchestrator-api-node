b8ac9a6d5ac716bcdd7408d3596a0ac8752c81c4
Masatomi KINO <masatomix@ki-no.org>
2020-02-22 11:55:02 +0900

    userテストを追加

--- src/samples/user/userSample.ts
+++ src/samples/user/userSample.ts
@@ -13,39 +13,42 @@ async function sample() {
     Name: 'user001',
     Surname: 'LastName001',
     UserName: `${random}`,
-    FullName: 'User LastName',
+    // FullName: 'User LastName', ココはName+Surnameで勝手に更新される
     EmailAddress: `${random}@example.com`,
     IsActive: true,
-    Password: 'afjlaf#adA0!',
+    Password: 'afjlaf#adA0!', // (初期状態は)割と強固なパスでないとはじかれる
     RolesList: ['Robot'],
   }
   let testUserId: number = NaN
   if (api.isEnterprise) {
     try {
-      // ユーザ作成
+      // サンプル ユーザ登録
       let testUser = await api.user.create(user)
       testUserId = testUser.Id
+      logger.info(testUser)
 
-      // 検索する
+      // サンプル ユーザ検索
       testUser = await api.user.find(testUserId)
       logger.info(testUser)
 
       const expectedName: string = '名前(更新成功)'
       testUser.Name = expectedName
 
-      // 更新をしてみる
+      // サンプル ユーザの更新
       testUser = await api.user.update(testUser)
-      // ちなみに、updateは戻り値は空
+      // ちなみに、updateは戻り値はundefined
       logger.info('---')
       logger.info(testUser)
       logger.info('---')
 
+      // そのユーザが検索できることを確認
       testUser = await api.user.find(testUserId)
       logger.info(testUser)
 
     } catch (error) {
       logger.error(error)
     } finally {
+      // サンプル ユーザ削除
       await api.user.delete(testUserId)
     }
   } else {
